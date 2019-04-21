38e4bcd8cbfbfd81ffac0f391e2b3dbde8487912
Luke McCulloch <tlukemcculloch@gmail.com>
2019-04-20 23:16:55 -0500

    initial commit - H X(not) and I gates; playing with qubits in tests

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # quantum_computation
 test driven development of a Quantum circuit machine using only Python and NumPy
+
+As an exercise, I am building a basic quntum circuits language from scratch.
---
+++ src/BraKet.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 14 15:30:03 2019
+
+@author: luke
+"""
+
+import numpy as np
+
+#import qutip.qip.gates
+
+
+isa = isinstance
+Symbol = str          # A q Symbol is implemented as a Python str
+List   = list         # A q List is implemented as a Python list
+array = np.ndarray
+Number = (int, float) # A Scheme Number is implemented as a Python int or float
+Matrix = (array)
+
+
+
+from complex_bit_def import cb0, cb1
+
+
+#def normalize(vector):
+#
+#    mag = np.linalg.norm(vector)
+#    return mag*vector
+
+def normalize(vector):
+    """
+    vector = H().array
+    cvector = vector.conj()
+    """
+    if np.size(vector) >2: 
+        cvector = vector.conj()
+        mag = np.sqrt( np.dot(cvector.T, vector) )
+        return (1./mag)*vector
+    
+def matrix_to_qubit_representaion(matrix, type_='C'):
+    return np.asarray([matrix.flatten(type_)]).T
+    
+
+class BraKet(object):
+    
+    def __init__(self, array, rep='ket', force_real_valued=False):
+        self.repmap = {'bra':0,'ket':1}
+        self.rep = rep
+        self.force_real_valued = force_real_valued
+        self.array = array
+        self.init_array(array)
+        
+#        self.mag = normalize(
+#                matrix_to_qubit_representaion(
+#                        self.array))
+    
+    def init_array(self, array):
+        if isa(array, list):
+            self.array = np.asarray([array]).T 
+            #if not self.force_real_valued:
+            #    self.array = np.asarray(array,complex).T
+            #else:
+            #    self.array = np.asarray(array,float).T 
+        elif isa(array, np.ndarray):
+            #if not self.force_real_valued:
+            self.array = array
+        else:
+            print 'ERROR, type is',type(array)
+        return 
+    
+    
+    
+    def __call__(self):
+        return self.array
+        
+    def __str__(self):
+        """print self"""
+        return self.rep+'\n{}'.format(self.array)
+    
+    def __repr__(self):
+        """>>> self"""
+        return self.rep+'\n{}'.format(self.array)
+        
+    def print_array(self):
+        return self.array
+    
+    def normalize(self):
+        return normalize(self.array)
+    
+    
+    def __add__(self,other):
+        """tensor (outer) product"""
+        if isa(other, BraKet):
+            return BraKet( self.array + other.array)
+        else:
+            return BraKet( self.array + other )
+            
+    
+    def __radd__(self, other):
+        return self.__add__(other)
+    
+    
+    def __mul__(self,other):
+        """tensor (outer) product"""
+        if isa(other, BraKet):
+            return BraKet( np.outer(self.array,other.array) )
+        else:
+            return BraKet( np.outer(self.array,other) )
+            
+    
+    def __rmul__(self, other):
+        return self.__mul__(other)
+    
+    
+    def elementwise_mul(self,other):
+        """tensor (outer) product"""
+        return BraKet( np.multiply(self.array,other.array ) )
+    
+
+     
+class qubit_factory(object):
+    
+    def __init__(self):
+        self.map = {'0' : BraKet([cb1,cb0]),
+                    '1' : BraKet([cb0,cb1]) 
+                    }
+    
+    def __call__(alpha = 1., beta = 1., zero=1., one = 1.):
+        
+        return np.copy(self.map[key])
+    
+class bloch_factory(object):
+    
+    def __init__(self):
+        self.map = {'0' : BraKet([cb1,cb0]),
+                    '1' : BraKet([cb0,cb1]) 
+                    }
+    
+    def __call__(alphs, theta, beta, phi):
+        return np.copy(self.map[key])
+        
